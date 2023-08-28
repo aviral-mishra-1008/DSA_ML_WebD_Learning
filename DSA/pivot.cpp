bb228d228d1ebec6bce79538dadde2ac3418d50a
Aviral <aviralmishra10@gmail.com>
2023-08-29 00:09:13 +0530

    Commit #9

--- DSA/pivot.cpp
+++ DSA/pivot.cpp
@@ -1,7 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int pivto(int arr[], int n){
+    int s=0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+    int check = 0;
+    while(s<e){
+        cout<<"Mid is : "<<mid<<endl;
+        int Leftsum = 0;
+        int Rightsum = 0;
+        for(int i = 0; i<mid; i++){
+            Leftsum = Leftsum + arr[i];
+            cout<<"value of i: "<<i<<endl;
+        }
+        cout<<"leftsum: "<<Leftsum<<endl;
 
+        for(int j = mid+1; j<n; j++ ){
+            Rightsum = Rightsum + arr[j];
+        }
+        cout<<"rightsum: "<<Rightsum<<endl;
+
+        if(Leftsum<0 || Rightsum<0){
+            if(Leftsum<Rightsum){
+                e = mid;
+            }
+
+
+            else{
+                e = mid
+            }
+        }
+        if(Leftsum==Rightsum){
+            check = 1;
+            break;
+        }
+
+        else if(Leftsum<Rightsum){
+            s = mid+1;
+            // cout<<"Yes";
+
+        }
+
+        else{
+            e = mid;
+            // cout<<"no";
+        }
+
+        mid = s+(e-s)/2;
+    }
+    if(check==1){
+            return mid;
+    }
+
+    else{
+        return -1;
+    }
+}
+int main(){
+    int arr[6] = {-1,-1,-1,-1,0,1};
+    int x = pivto(arr,6);
+    cout<<x;
     return 0;
 }
--- DSA/test.py
+++ DSA/test.py
@@ -1,23 +1,40 @@
-import numpy as np
-lis = [1,2,2,4,4,4]
-counter = []
-count = []
-check = 0
-for i in lis:
-    if i in counter:
-        continue
-    else:
-        counter.append(i)
-        x = lis.count(i)
-        if x in count:
-            check = 1
-            break
-        else:
-            count.append(x)
+# import numpy as np
+# lis = [1,2,2,4,4,4]
+# counter = []
+# count = []
+# check = 0
+# for i in lis:
+#     if i in counter:
+#         continue
+#     else:
+#         counter.append(i)
+#         x = lis.count(i)
+#         if x in count:
+#             check = 1
+#             break
+#         else:
+#             count.append(x)
 
-if check==0:
-    return(True)
+# if check==0:
+#     return(True)
 
-elif check==1:
-    return(False)
+# elif check==1:
+#     return(False)
 
+while(True):
+    M = float(input("Mass: "))
+    # Z = float(input("Rank: "))
+    Na = 6.022
+    density = float(input("Density: "))
+    # if Z==4:
+    #     a = 2*((2)**(0.5))*r*(10**(-10))
+    #     e = a**3
+
+    # else:
+    #     a = 4*r/(3**(0.5))
+    #     f = a*(10**(-10))
+    #     e = f**3
+
+    ans = Na*density/M
+
+    print(ans)
