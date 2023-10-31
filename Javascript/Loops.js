33640c9441c01d95b87241229652287d08e6f06c
Aviral <aviralmishra10@gmail.com>
2023-11-01 00:16:35 +0530

    Commit #27

---
+++ Javascript/Loops.js
@@ -0,0 +1,11 @@
+//Let's have a look at the loops 
+for(let i=0; i<5; i++){
+    console.log(i);
+}
+
+//Now let's see while loop format
+
+let i = 0;
+while(i<3){
+    console.log(i);
+}
---
+++ Javascript/arrays.js
@@ -0,0 +1,24 @@
+//This is how you save data in the js
+//ARRAYS
+
+const arr = ['India', 'Russia', 'China'];
+
+console.log(arr);
+console.log(arr[1]); //negative indexing doesn't work
+
+//unlike c++/java we can think of arrays as lists in python
+//you can also add function and object into the array
+
+//There are a number of properties and methods in this 
+
+console.log(arr.length);  //functions/methods require () properties don't!
+
+//indexOf() tells index of element
+//includes() is like the membership operator
+//push() adds new element to the end of an array and returns new lenght of array
+//unshift() adds a new element to the beggining of an array and returns new length of array
+// pop() removes the last element of array and returns removed element
+// shift() removes first element and returns removed element
+//sort() sorts th elements in alphabetic and ascending order
+// slice() selects part of ana array and return new array
+
